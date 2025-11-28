2bee457031a9bcc6ab9b2d64131fe6e9886082a0
Yağmur <yagmur_ssezay@hotmail.com>
2025-11-28 18:12:18 +0300

    getData()'nın calismama durumu gosterildi

--- src/abstractDemo/Main.java
+++ src/abstractDemo/Main.java
@@ -2,6 +2,7 @@ package abstractDemo;
 
 public class Main {
     public static void main(String[] args) {
-
+    CustomerManager customerManager = new CustomerManager();
+    customerManager.getCustomers();
     }
 }
