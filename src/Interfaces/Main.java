18718645c80f82f9e211abf3e73a9e61b09be7c8
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-01 14:09:17 +0300

    kendisini new'lemek uygun değil o yüzden onun yerine kendisini implemente eden classı new'leriz

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -2,11 +2,6 @@ package Interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        ICustomerDal customerDal = new ICustomerDal() {
-            @Override
-            public void Add() {
-
-            }
-        };
+        ICustomerDal customerDal = new OracleCustomerDal();
     }
 }
