4f5e266d2242b8b89a147ec88235fefba48d69bb
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-01 14:06:23 +0300

    interfaceler de abstract classlar gibi new'lenmiyor, düzgün bir yapı oluşmuyor

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -2,6 +2,11 @@ package Interfaces;
 
 public class Main {
     public static void main(String[] args) {
+        ICustomerDal customerDal = new ICustomerDal() {
+            @Override
+            public void Add() {
 
+            }
+        };
     }
 }
