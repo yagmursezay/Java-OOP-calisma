ea7448cec50ff5642d2682f58d2d5ebf9df7924b
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-01 16:36:20 +0300

    ProductManager classında ProductValidator'ı newlemeden if bloğunda kullanımını gösterdik. ancak ProductValidator classındaki isValid fonksiyonunun static olması gerektiğini belirttik.

--- src/StaticDemo/ProductManager.java
+++ src/StaticDemo/ProductManager.java
@@ -2,8 +2,8 @@ package StaticDemo;
 
 public class ProductManager {
     public void Add(Product product) {
-        ProductValidator validator = new ProductValidator();
-        if(validator.isValid(product)) {
+
+        if(ProductValidator.isValid(product)) {
             System.out.println("Eklendi.");
         }else{
             System.out.println("Ürün bilgileri geçersizdir.");
--- src/StaticDemo/ProductValidator.java
+++ src/StaticDemo/ProductValidator.java
@@ -2,7 +2,7 @@ package StaticDemo;
 
 public class ProductValidator {
     //bir ürünü kaydederken veya güncellerken ürünün kurallara uygun olup olmadığını bulmak için kullanılır
-    public boolean isValid(Product product) {
+    public static boolean isValid(Product product) {
         if(product.price > 0 && !product.name.isEmpty() ){
             return true;
         }
