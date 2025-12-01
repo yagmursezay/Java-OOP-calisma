ed1b36188e0c4c27d8a59a5502c42bf27d083574
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-01 16:19:47 +0300

    manager classı oluşturdu ve ProductValidator classındaki IsValid true ise eklendi çıktısını ver

---
+++ src/StaticDemo/ProductManager.java
@@ -0,0 +1,10 @@
+package StaticDemo;
+
+public class ProductManager {
+    public void Add(Product product) {
+        ProductValidator validator = new ProductValidator();
+        if(validator.isValid(product)) {
+            System.out.println("Eklendi.");
+        }
+    }
+}
