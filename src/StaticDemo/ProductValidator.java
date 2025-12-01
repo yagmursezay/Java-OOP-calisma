5790b1fcb78da2139612d90bc4faed9c7212e489
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-01 16:48:12 +0300

    Static yapıcı blok yapısı gösterildi.

--- src/StaticDemo/ProductValidator.java
+++ src/StaticDemo/ProductValidator.java
@@ -1,22 +1,24 @@
 package StaticDemo;
 
 public class ProductValidator {
-
+    static {
+        System.out.println("Static yapıcı blok çalıştı.");
+    }
     public ProductValidator() {//constructor newlenince çalışır
         System.out.println("Yapıcı blok çalıştı.");
     }
 
     //bir ürünü kaydederken veya güncellerken ürünün kurallara uygun olup olmadığını bulmak için kullanılır
     public static boolean isValid(Product product) {
-        if(product.price > 0 && !product.name.isEmpty() ){
+        if (product.price > 0 && !product.name.isEmpty()) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
 
     }
-    public void birsey(){
+
+    public void birsey() {
 
     }
 }
