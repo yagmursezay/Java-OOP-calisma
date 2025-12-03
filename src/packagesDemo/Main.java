bae8d37510c27f84448d62f8fbd1a426e410daba
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-03 15:54:24 +0300

    scanner paketi üzerinden örnek gösterildi

--- src/packagesDemo/Main.java
+++ src/packagesDemo/Main.java
@@ -2,4 +2,15 @@ package packagesDemo;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adınız:");
+
+        String isim = scanner.nextLine();
+
+        System.out.println("Merhaba " +isim);
+
+
+    }
 }
