6125186d787731a7f141113872d503f566f4a66b
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-04 16:31:03 +0300

    new int ile yeni boyut yazıldığı anda üstte tanımlanan dizi çöpe gider, yeniden oluşturulur. default olarak da 0. indexte 0 vardır.

---
+++ src/arrayListDemo/Main.java
@@ -0,0 +1,9 @@
+package arrayListDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] sayilar = new int[] {1,2,3};
+        sayilar = new int[4];
+        System.out.println(sayilar[0]);
+    }
+}
