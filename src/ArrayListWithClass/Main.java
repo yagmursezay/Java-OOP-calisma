63c1f1c87e4b79b7de6bd787023a088c32b0da25
Yağmur <yagmur_ssezay@hotmail.com>
2025-12-04 17:22:56 +0300

    Customer classı oluşturuldu ve Customer türünden ArrayList oluşturma gösterildi.

---
+++ src/ArrayListWithClass/Main.java
@@ -0,0 +1,15 @@
+package ArrayListWithClass;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+
+        customers.add(new Customer (1, "Yağmur", "Sezay"));
+        customers.add(new Customer (2, "Baran", "Gürkan"));
+        customers.add(new Customer (3, "Muri", "Gürkan"));
+
+
+    }
+}
